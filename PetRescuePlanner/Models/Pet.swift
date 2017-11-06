14b299ef828497263b8ea0e5548bf3e4d02ab11a
Daniel Rodosky <djrodosky@gmail.com>
2017-11-06 10:43:08 -0700

    added pet model file

---
+++ PetRescuePlanner/Models/Pet.swift
@@ -0,0 +1,14 @@
+//
+//  Pet.swift
+//  PetRescuePlanner
+//
+//  Created by Daniel Rodosky on 11/6/17.
+//  Copyright © 2017 Daniel Rodosky. All rights reserved.
+//
+
+import Foundation
+
+
+struct Pet {
+    
+}
