ec3119ef9cc47a02b18d092932e2cfcb87f910c7
Brian Licea <brianlicea@Brians-MacBook-Air.local>
2017-11-06 15:56:26 -0700

    add shelter modelstarted the ShelterController

---
+++ PetRescuePlanner/Models/Shelter.swift
@@ -0,0 +1,9 @@
+//
+//  Shelter.swift
+//  PetRescuePlanner
+//
+//  Created by Brian Licea on 11/6/17.
+//  Copyright © 2017 Daniel Rodosky. All rights reserved.
+//
+
+import Foundation
---
+++ PetRescuePlanner/Models/ShelterController.swift
@@ -0,0 +1,9 @@
+//
+//  ShelterController.swift
+//  PetRescuePlanner
+//
+//  Created by Brian Licea on 11/6/17.
+//  Copyright © 2017 Daniel Rodosky. All rights reserved.
+//
+
+import Foundation
