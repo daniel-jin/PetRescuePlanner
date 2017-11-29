2ee158bf6b96ebfcf5252496b276f06ced6f21e2
danjinandjuice <daniel.h.jin@gmail.com>
2017-11-29 15:17:46 -0700

    Saved pets will be in order of last saved, even across multiple devices

--- PetRescuePlanner/ViewControllers/SavedPetsListTableViewController.swift
+++ PetRescuePlanner/ViewControllers/SavedPetsListTableViewController.swift
@@ -59,6 +59,14 @@ class SavedPetsListTableViewController: UITableViewController {
 
             let petToDelete = PetController.shared.savedPets[indexPath.row]
             
+            // Delete from sorted array to update iCloud key/value store
+            
+            guard let petID = petToDelete.id,
+                let index = PetController.shared.sortedPetArray.index(of: petID) else { return }
+            PetController.shared.sortedPetArray.remove(at: index)
+            PetController.shared.saveToiCloud()
+            
+            
             // Delete from Core Data
             
             PetController.shared.delete(pet: petToDelete, completion: {
