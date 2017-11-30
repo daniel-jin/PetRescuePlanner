21c8ef3aea3002b0ccee89d2dd18eea35dc90cb9
Daniel Rodosky <djrodosky@gmail.com>
2017-11-30 16:38:03 -0700

    Commented out some debugging code

--- PetRescuePlanner/Sources/AppDelegate.swift
+++ PetRescuePlanner/Sources/AppDelegate.swift
@@ -34,14 +34,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             NSAttributedStringKey.font: themeFont,
             NSAttributedStringKey.foregroundColor: UIColor.white]
         navBar.tintColor = UIColor.white
-        /******* DEBUG CODE, DELETE LATER ******/
-        let moc = CoreDataStack.context
-        let nc = NotificationCenter.default
-        nc.addObserver(forName: .NSManagedObjectContextObjectsDidChange, object: moc, queue: nil) { (note) in
-            if let insertedObjects = note.userInfo?[NSInsertedObjectsKey] as? Set<AnyHashable> {
-                print("inserted \(insertedObjects)")
-            }
-        }
+//        /******* DEBUG CODE, DELETE LATER ******/
+//        let moc = CoreDataStack.context
+//        let nc = NotificationCenter.default
+//        nc.addObserver(forName: .NSManagedObjectContextObjectsDidChange, object: moc, queue: nil) { (note) in
+//            if let insertedObjects = note.userInfo?[NSInsertedObjectsKey] as? Set<AnyHashable> {
+//                print("inserted \(insertedObjects)")
+//            }
+//        }
         
         return true
     }
